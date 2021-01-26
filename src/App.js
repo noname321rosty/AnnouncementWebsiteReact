a9fffdae657f0df90d899ee0c74bb66958a30e31
noname321rosty <bilka2001@gmail.com>
2021-01-26 15:18:16 +0200

    add yarn deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route, Redirect, NavLink} from 'react-router-dom';
 import Task from "./components/task/Task";
 import Announcements from "./components/announcements/Announcements";
-import Edit from "./components/edit/Edit";
 import Announcement from "./components/announcement/Announcement";
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
                 <Route path={'/announcements/:id'} component={Announcement}/>
                 <Route path={'/announcements'} component={Announcements}/>
                 <Route path={'/task'} component={Task}/>
-                {/*<Route path={'/edit'} component={Edit}/>*/}
                 <Redirect from='/' to='/task'/>
             </Switch>
         </Router>
